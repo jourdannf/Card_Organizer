97f8d64bdf5dba1c18374b7be3bef19fc0bada29
jourdannf <jourdann.fraser@gmail.com>
2024-07-13 00:27:21 -0400

    Added data structures

---
+++ data/cards.js
@@ -0,0 +1,64 @@
+const cards = [
+    {
+        userId: 203,
+        name: "Choi Yujin",
+        group: "Kep1er",
+        album: "Troubleshooter",
+        year: "2022",
+        rarity: 5,
+        imgLink: "",
+        collect: true
+    },
+    {
+        userId: 203,
+        name: "Songsun",
+        group: "TRI.BE",
+        album: "Diamond",
+        year: "2024",
+        rarity: 2,
+        imgLink: "",
+        collect: true
+    },
+    {
+        userId: 204,
+        name: "Choi Yujin",
+        group: "Kep1er",
+        album: "Troubleshooter",
+        year: "2022",
+        rarity: 5,
+        imgLink: "",
+        collect: false
+    },
+    {
+        userId: 205,
+        name: "Youha",
+        group: "",
+        album: "ISLAND",
+        year: "2020",
+        rarity: 1,
+        imgLink: "",
+        collect: true
+    },
+    {
+        userId: 205,
+        name: "Seulgi",
+        group: "",
+        album: "28 Reasons",
+        year: "2023",
+        rarity: 1,
+        imgLink: "",
+        collect: true
+    },
+    {
+        userId: 205,
+        name: "Seulgi",
+        group: "Red Velvet",
+        album: "The Perfect Red Velvet",
+        year: "2018",
+        rarity: 3,
+        imgLink: "",
+        collect: false
+    }
+];
+
+module.exports = cards;
---
+++ data/users.js
@@ -0,0 +1,28 @@
+const users = [
+    {
+        userId: 203,
+        name: Jourdann,
+        profilePic: "",
+        cardsCollected: 2,
+        bio: "I'm just a person who loves collecting cards!",
+        password: "bigBen123"
+    },
+    {
+        userId: 204,
+        name: Cameron,
+        profilePic: "",
+        cardsCollected: 0,
+        bio: "",
+        password: "tw1c3girls"
+    },
+    {
+        userId: 205,
+        name: Amber,
+        profilePic: "",
+        cardsCollected: 3,
+        bio: "DM for selling opportunities...",
+        password: "troubleMaker87"
+    }
+];
+
+module.exports = users;
